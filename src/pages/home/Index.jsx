7e0645ad63aa5269d329e573ff3e1f62a228aad3
Kaike Martins <kaikesilvamartins10.com>
2026-03-08 14:48:11 -0300

    ajuste

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -1,4 +1,5 @@
 import './home.css';
+import foto from "../../assets/KaikeFoto.jpeg";
 
 function Index() {
   return (
@@ -23,7 +24,7 @@ function Index() {
       </div>
 
       <div className="hero-img">
-        <img src="src/assets/kaikeFoto.jpeg" alt="Kaike Martins"/>
+        <img src={foto} alt="Kaike Martins"/>
       </div>
     </section>
     </>
